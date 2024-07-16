5ff088b6727847343c6c8b805762efa43879ddad
MacBook Pro <macbookpro@Ziyods-MacBook-Pro.local>
2024-07-16 20:06:08 +0500

    fixed

--- src/types/service.tsx
+++ src/types/service.tsx
@@ -5,14 +5,14 @@ interface Service {
 }
 
 interface ServiceModal2 {
-  id?: number; // Assuming id can be optional or undefined
+  id?: number;
   name: string;
-  price: number | string; // Price can be either number or string, as per your requirement
+  price: number | string;
 }
 
 interface ServiceEdit {
   name: string;
-  price: string | number; // Price can be either number or string during edit
+  price: string | number;
 }
 
 interface ServiceModalProps {
